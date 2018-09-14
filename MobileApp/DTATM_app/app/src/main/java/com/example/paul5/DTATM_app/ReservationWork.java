3dcc97e54f737fb9a41eb3a952161d341a3ebd5c
Peagsus-69 <h01090995964@gmail.com>
2018-09-14 16:10:50 +0900

    커밋했음

--- MobileApp/DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/ReservationWork.java
+++ MobileApp/DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/ReservationWork.java
@@ -2,7 +2,7 @@ package com.example.paul5.DTATM_app;
 
 public class ReservationWork {
     private String businessName;
-
+    private int test;
 
     public void setBusinessName(String businessName) {
         this.businessName = businessName;
