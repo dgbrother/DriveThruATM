085b9172ffba058dfb82fcc325ba93069044206e
Peagsus-69 <h01090995964@gmail.com>
2018-09-14 16:06:16 +0900

    영래 커밋

--- MobileApp/DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/ReservationWork.java
+++ MobileApp/DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/ReservationWork.java
@@ -2,6 +2,7 @@ package com.example.paul5.DTATM_app;
 
 public class ReservationWork {
     private String businessName;
+    private String testbranch;
 
     public void setBusinessName(String businessName) {
         this.businessName = businessName;
