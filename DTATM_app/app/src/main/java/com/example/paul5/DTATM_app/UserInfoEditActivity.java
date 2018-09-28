ea70d4f4a68064a97670945c2ebf7c28d57c49d3
YoungJinAn <paul26375@gmail.com>
2018-09-29 03:51:49 +0900

    서버 DB에서 user info를 가져와 EditText에 출력 수정/저장 버튼을 사용해 수정기능 활성화/비활성화 및 수정내용 저장 수정내용 DB에 저장하는 부분은 아직 미완성

--- DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/UserInfoEditActivity.java
+++ DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/UserInfoEditActivity.java
@@ -2,41 +2,91 @@ package com.example.paul5.DTATM_app;
 
 import android.content.ContentValues;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.concurrent.ExecutionException;
-
-public class UserInfoEditActivity extends AppCompatActivity {
+public class UserInfoEditActivity extends AppCompatActivity implements View.OnClickListener {
     private final String SERVER_URL = "http://35.200.117.1:8080/control_json.jsp";
-    EditText eUserName;
-    UserInfo userInfo;
+    EditText eUserName, eUserID, eUserPassword, eUserEmail;
+    EditText eUserAccount, eUserCarNumber, eUserNFCID;
+    Button editButton, okButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_edit);
 
+        eUserName       = findViewById(R.id.userInfo_userName_editText);
+        eUserID         = findViewById(R.id.userInfo_userId_editText);
+        eUserPassword   = findViewById(R.id.userInfo_password_editText);
+        eUserEmail      = findViewById(R.id.userInfo_email_editText);
+        eUserAccount    = findViewById(R.id.userInfo_account_editText);
+        eUserCarNumber  = findViewById(R.id.userInfo_carNumber_editText);
+        eUserNFCID      = findViewById(R.id.userInfo_nfcId_editText);
+
+        editButton      = findViewById(R.id.userInfo_edit_button);
+        okButton        = findViewById(R.id.userInfo_ok_button);
+        editButton      .setOnClickListener(this);
+        okButton        .setOnClickListener(this);
+
+        // 원래는 로그인 정보 가져옴
         String userId = "ID1234";
-        userInfo = getUserInfo(userId);
 
-        eUserName = findViewById(R.id.userInfo_userName_editText);
-    }
-
-    private UserInfo getUserInfo(String userId) {
         ContentValues params = new ContentValues();
         params.put("action", "userinfo");
         params.put("userid", userId);
 
+        NetworkTask getUserInfoTask = new NetworkTask(SERVER_URL, params);
+        getUserInfoTask.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.userInfo_edit_button:
+                editUserInfo();
+                break;
+            case R.id.userInfo_ok_button:
+                finish();
+                break;
+        }
+    }
+
+    private class NetworkTask extends AsyncTask<Void, Void, UserInfo> {
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected UserInfo doInBackground(Void... voids) {
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            JSONObject jsonResult = requestHttpURLConnection.request(url, values);
+
+            UserInfo userInfo = jsonToUserInfo(jsonResult);
+            return userInfo;
+        }
+
+        @Override
+        protected void onPostExecute(UserInfo userInfo) {
+            super.onPostExecute(userInfo);
+            setUserInfo(userInfo);
+            setEnables(false);
+        }
+    }
+
+    private UserInfo jsonToUserInfo(JSONObject jsonObject) {
         try {
-            NetworkTask networkTask = new NetworkTask(SERVER_URL, params);
-            JSONObject jsonObject = networkTask.execute().get();
             JSONArray jsonArray = jsonObject.getJSONArray("data");
 
             JSONObject jobj = jsonArray.getJSONObject(0);
@@ -50,37 +100,53 @@ public class UserInfoEditActivity extends AppCompatActivity {
                     jobj.getString("carnumber"),
                     jobj.getString("nfc")
             );
-
             return userInfo;
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private class NetworkTask extends AsyncTask<Void, Void, JSONObject> {
-        private String url;
-        private ContentValues values;
+    // 유저 정보 수정
+    private void editUserInfo() {
+        String status = editButton.getText().toString();
 
-        public NetworkTask(String url, ContentValues values) {
-            this.url = url;
-            this.values = values;
+        /**
+         * '수정' 버튼을 누르면 edit Text가 수정 가능하게 바뀌고
+         * 버튼 text 가 '저장'으로 바뀜
+         * 수정 중일 때에는 완료 버튼 비활성화
+         */
+        if(status.equals("수정")) {
+            setEnables(true);
+            editButton.setText("저장");
+            okButton.setEnabled(false);
         }
+        if(status.equals("저장")) {
+            setEnables(false);
+            editButton.setText("수정");
+            okButton.setEnabled(true);
+        }
+    }
 
-        @Override
-        protected JSONObject doInBackground(Void... voids) {
-            JSONObject result;
-            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, values);
-            return result;
-        }
+    /// edit Text를 유저 정보로 채움
+    private void setUserInfo(UserInfo userInfo) {
+        eUserName       .setText(userInfo.getName());
+        eUserID         .setText(userInfo.getId());
+        eUserPassword   .setText(userInfo.getPassword());
+        eUserEmail      .setText(userInfo.getEmail());
+        eUserAccount    .setText(userInfo.getAccount());
+        eUserCarNumber  .setText(userInfo.getCarNumber());
+        eUserNFCID      .setText(userInfo.getNfcId());
+    }
 
-        @Override
-        protected void onPostExecute(JSONObject s) {
-            super.onPostExecute(s);
-            eUserName.setText(userInfo.getName());
-        }
+    // edit Text의 활성화 토글
+    private void setEnables(boolean enables) {
+        eUserName       .setEnabled(enables);
+        eUserID         .setEnabled(enables);
+        eUserPassword   .setEnabled(enables);
+        eUserEmail      .setEnabled(enables);
+        eUserAccount    .setEnabled(enables);
+        eUserCarNumber  .setEnabled(enables);
+        eUserNFCID      .setEnabled(enables);
     }
 }
