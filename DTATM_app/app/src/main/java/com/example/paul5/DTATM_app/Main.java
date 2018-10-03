a80b47e168bd53eab4595fee273ba6e9bfa24b67
Peagsus-69 <h01090995964@gmail.com>
2018-10-03 16:54:55 +0900

    로그인 화면 로고추가, 예약 메인화면 추가(뒤로가기, 로그아웃 구현완료)

--- DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/Main.java
+++ DTATM_app/app/src/main/java/com/example/paul5/DTATM_app/Main.java
@@ -14,10 +14,10 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button reserveBtn = findViewById(R.id.reserve);
-        Button searchBtn = findViewById(R.id.search);
-        Button usereditBtn = findViewById(R.id.user_edit);
-        Button logoutBtn = findViewById(R.id.logout);
+        Button reserveBtn = findViewById(R.id.reserveBtn);
+        Button searchBtn = findViewById(R.id.searchBtn);
+        Button usereditBtn = findViewById(R.id.user_editBtn);
+        Button logoutBtn = findViewById(R.id.logoutBtn);
         reserveBtn.setOnClickListener(this);
         searchBtn.setOnClickListener(this);
         usereditBtn.setOnClickListener(this);
@@ -27,15 +27,19 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.reserve :
+            case R.id.reserveBtn :
+                Intent intent = new Intent(Main.this, Reserve.class);
+                startActivity(intent);
                 break;
-            case R.id.search :
+            case R.id.searchBtn :
+                //Intent intent2 = new Intent(Main.this, Search.class);
+                //startActivity(intent2);
                 break;
-            case R.id.user_edit :
+            case R.id.user_editBtn :
                 Intent intent3 = new Intent(Main.this, UserInfoEditActivity.class);
                 startActivity(intent3);
                 break;
-            case R.id.logout :
+            case R.id.logoutBtn :
                 Intent intent4 = new Intent(Main.this, Login.class);
                 startActivity(intent4);
                 Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
